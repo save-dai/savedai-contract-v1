f1d843276d3bd8e5c2ca0730769baf72f7f15625
Mehmet Alkanlar <mralkanlar@gmail.com>
2020-02-15 23:39:50 -0700

    Migrations

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -1,19 +1,30 @@
 import React from "react";
-/*
+
 import {
   AccountData,
   ContractData,
   ContractForm,
 } from "@drizzle/react-components";
-*/
+
 import logo from "./logo.png";
 
 export default ({ accounts }) => (
   <div className="App">
     <div className="section">
-      <h2>Get Quote</h2>
+      <h2>Buy SaveDAI</h2>
 
     </div>
-
+    <div className="section">
+      <h2>SimpleStorage</h2>
+      <p>
+        This shows a simple ContractData component with no arguments, along with
+        a form to set its value.
+      </p>
+      <p>
+        <strong>Stored Value: </strong>
+        <ContractData contract="SimpleStorage" method="storedData" />
+      </p>
+      <ContractForm contract="SimpleStorage" method="set" />
+    </div>
   </div>
 );
--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,14 +1,15 @@
 import Web3 from "web3";
-//import SimpleStorage from "./contracts/SimpleStorage.json";
+import SimpleStorage from "./contracts/SimpleStorage.json";
+import SaveDAI from "./contracts/SaveDAI.json";
 //import ComplexStorage from "./contracts/ComplexStorage.json";
 // import TutorialToken from "./contracts/TutorialToken.json";
 
 
 const options = {
-  contracts: [],
+  contracts: [SimpleStorage, SaveDAI],
   //contracts: [SimpleStorage, ComplexStorage, TutorialToken],
   events: {
-    //SimpleStorage: ["StorageSet"],
+    SimpleStorage: ["StorageSet"],
   },
   polls: {
     accounts: 1500,
