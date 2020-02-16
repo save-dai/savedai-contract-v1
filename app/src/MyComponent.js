76c3b4a98f264eac3779a5153357497a6b897303
Mehmet Alkanlar <mralkanlar@gmail.com>
2020-02-16 00:05:11 -0700

    Fix label, remove other contract

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -12,21 +12,10 @@ export default ({ accounts }) => (
   <div className="App">
     <div className="section">
       <h2>Exercise SaveDAI And Get Back DAI</h2>
-      <ContractForm contract="SaveDAI" method="exerciseOCDAI" labels={["Dai Amount"]}/>
+      <ContractForm contract="SaveDAI" method="exerciseOCDAI" labels={["SaveDAI Amount"]}/>
       <h2>Get SaveDAI</h2>
       <ContractForm contract="SaveDAI" method="mint" labels={["Recipient Address", "Dai Amount"]}/>
     </div>
-    <div className="section">
-      <h2>SimpleStorage</h2>
-      <p>
-        This shows a simple ContractData component with no arguments, along with
-        a form to set its value.
-      </p>
-      <p>
-        <strong>Stored Value: </strong>
-        <ContractData contract="SimpleStorage" method="storedData" />
-      </p>
-      <ContractForm contract="SimpleStorage" method="set" />
-    </div>
+
   </div>
 );
