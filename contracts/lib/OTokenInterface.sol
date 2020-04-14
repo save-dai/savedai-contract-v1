e178c9df2b3a848ab4e4e674f788ae764370f9bb
Kseniya Lifanova <kseniya@upstateinteractive.io>
2020-04-14 11:45:55 -0700

    updated interface function

--- contracts/lib/OTokenInterface.sol
+++ contracts/lib/OTokenInterface.sol
@@ -3,14 +3,11 @@ pragma solidity ^0.5.0;
 // opyn interface
 interface OTokenInterface {
     function hasExpired() external view returns (bool);
-    function exercise(uint256 _amount, address payable[] memory vaultsToExerciseFrom) external payable;
+    function exercise(uint256 _amount, address payable[] calldata vaultsToExerciseFrom) external payable;
     function isExerciseWindow() external view returns (bool);
-    function getVaultOwners() public view returns (address payable[] memory);
-    function hasVault(address payable owner) public view returns (bool);
-    function numVaults() public view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function oTokenExchangeRate() external returns (uint256, int32);
     function balanceOf(address account) external view returns (uint256);
     function totalSupply() external view returns (uint256);
-    function underlyingRequiredToExercise(uint256 oTokensToExercise) public view returns (uint256);
+    function underlyingRequiredToExercise(uint256 oTokensToExercise) external view returns (uint256);
 }
