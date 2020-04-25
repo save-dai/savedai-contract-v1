18d7cb19fa4dfa339c7a9a27c3b38ea1d7b6a728
Kseniya Lifanova <kseniya@upstateinteractive.io>
2020-04-25 16:11:36 -0700

    added secrets file, updated config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,16 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-
-require('dotenv').config();
+const fs = require('fs');
 
 const path = require('path');
+let secrets;
+let projectId;
+let mnemonic = '';
+
+if (fs.existsSync('./secrets.json')) {
+  secrets = require('./secrets.json');
+  mnemonic = secrets.mnemonic;
+  projectId = secrets.projectId;
+}
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,10 +28,20 @@ module.exports = {
       gas: 6000000,
     },
     kovan: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`),
       network_id: 42,
       gas: 3000000,
       gasPrice: 10000000000,
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${projectId}`),
+      network_id: 1,       // Mainnet id
+      chain_id: 1,
+      gas: 8000000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 2000,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,    // Skip dry run before migrations? (default: false for public nets )
+      gasPrice: 7000000000,  // 7 gwei (in wei) (default: 100 gwei)
     },
   },
 };
