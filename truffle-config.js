f41328a131d0da90c6af7f29fc4ce163f10990b5
Kseniya Lifanova <kseniya@upstateinteractive.io>
2020-05-20 12:24:53 -0700

    added abi folder

--- truffle-config.js
+++ truffle-config.js
@@ -15,7 +15,6 @@ if (fs.existsSync('./secrets.json')) {
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, 'app/src/contracts'),
   networks: {
     develop: {
       port: 8545,
