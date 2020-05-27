2aa97e4187468f11fc622bee233c73412cd44a10
Kseniya Lifanova <kseniya@upstateinteractive.io>
2020-05-26 21:24:08 -0700

    added rinkeby to truffle config file

--- truffle-config.js
+++ truffle-config.js
@@ -32,6 +32,15 @@ module.exports = {
       gas: 3000000,
       gasPrice: 10000000000,
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
+      network_id: 4,       // Rinkeby's id
+      gas: 10000000,        // Rinkeby has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 2000,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,    // Skip dry run before migrations? (default: false for public nets )
+      gasPrice: 7000000000, // 7 gwei (in wei) (default: 100 gwei)
+    },
     mainnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${projectId}`),
       network_id: 1,       // Mainnet id
